33b292f57b474bb94eacb8a1f751a966d0f3b204
Strange3228 <51462607+Strange3228@users.noreply.github.com>
2020-07-06 18:29:00 +0200

    A little styling, full playable music

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     let beat13 = document.getElementById("beat_13");
     let beat14 = document.getElementById("beat_14");
     let beat15 = document.getElementById("beat_15");
+
     let place1 = document.getElementById("place_for_music1");
     let place2 = document.getElementById("place_for_music2");
     let place3 = document.getElementById("place_for_music3");
@@ -21,12 +22,87 @@ $(document).ready(function(){
     let place5 = document.getElementById("place_for_music5");
 
     var beatInterval1 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval2 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval3 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval4 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval5 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval6 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval7 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval8 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval9 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval10 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval11 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval12 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval13 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval14 = setInterval(function(){console.log("tratatata");}, 100000);
+    var beatInterval15 = setInterval(function(){console.log("tratatata");}, 100000);
 
     function playAudio(beat){
-      beatInterval1 = setInterval(function(){beat.play();}, 10)
+      if(beat === beat1){
+        beatInterval1 = setInterval(function(){beat.play();}, 10)
+      } else if (beat === beat2){
+        beatInterval2 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat3){
+        beatInterval3 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat4){
+        beatInterval4 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat5){
+        beatInterval5 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat6){
+        beatInterval6 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat7){
+        beatInterval7 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat8){
+        beatInterval8 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat9){
+        beatInterval9 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat10){
+        beatInterval10 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat11){
+        beatInterval11 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat12){
+        beatInterval12 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat13){
+        beatInterval13 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat14){
+        beatInterval4 = setInterval(function(){beat.play();}, 10)
+      }else if (beat === beat15){
+        beatInterval15 = setInterval(function(){beat.play();}, 10)
+      }
     }
-    function stopAudio(){
-        clearInterval(beatInterval1)
+
+    function stopAudio(beat){
+      if(beat === document.getElementById("movable_div1")){
+        clearInterval(beatInterval1);
+      } else if (beat === document.getElementById("movable_div2")){
+        clearInterval(beatInterval2);
+      } else if (beat === document.getElementById("movable_div3")){
+        clearInterval(beatInterval3);
+      } else if (beat === document.getElementById("movable_div4")){
+        clearInterval(beatInterval4);
+      } else if (beat === document.getElementById("movable_div5")){
+        clearInterval(beatInterval5);
+      } else if (beat === document.getElementById("movable_div6")){
+        clearInterval(beatInterval6);
+      } else if (beat === document.getElementById("movable_div7")){
+        clearInterval(beatInterval7);
+      } else if (beat === document.getElementById("movable_div8")){
+        clearInterval(beatInterval8);
+      } else if (beat === document.getElementById("movable_div9")){
+        clearInterval(beatInterval9);
+      } else if (beat === document.getElementById("movable_div10")){
+        clearInterval(beatInterval10);
+      } else if (beat === document.getElementById("movable_div11")){
+        clearInterval(beatInterval11);
+      } else if (beat === document.getElementById("movable_div12")){
+        clearInterval(beatInterval12);
+      } else if (beat === document.getElementById("movable_div13")){
+        clearInterval(beatInterval13);
+      } else if (beat === document.getElementById("movable_div14")){
+        clearInterval(beatInterval14);
+      } else if (beat === document.getElementById("movable_div15")){
+        clearInterval(beatInterval15);
+      }
     }
 
     dragElement(document.getElementById("movable_div1"));
@@ -106,7 +182,7 @@ $(document).ready(function(){
             }
 
             else{
-              stopAudio();
+              stopAudio(elmnt);
               elmnt.style.left = start_place_left + "px";
               elmnt.style.top = start_place_top + "px";
             }
